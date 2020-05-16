d96b5e7235ef82fdad71d3e6c1e1686dbc1df115
Jose Antonio Hernandez <jahco1980@gmail.com>
2020-05-16 10:42:55 -0700

    My last commit for this project.

--- AnIrisGraph.R
+++ AnIrisGraph.R
@@ -4,7 +4,7 @@ sd(iris$Sepal.Length)
 my.plot <- ggplot(data = iris, 
        aes(x = Sepal.Length,
            y = Sepal.Width, color = Species)) +
-  geom_point(size = 5) +
-  ggtitle("My iris graph 2")
+  geom_point(size = 7) +
+  ggtitle("This is a beautiful graph")
 
 ggsave(filename = "~/Desktop/MyNewProject/myIrisgraph.png")
